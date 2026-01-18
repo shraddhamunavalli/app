f8b1d37119e2048c5c49954539d16e49955e58a5
shraddhamunavalli <munavallishraddha4@gmail.com>
2026-01-18 21:27:26 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import square
 def test_square():
-    result = square(5)
-    print("Square of 5 =", result)
-    assert result == 25
+result = square(5)
+print("Square of 5 =", result)
+assert result == 25
