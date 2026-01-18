88bbd6976da7dd1cd1cff3b856a265697ba0a7f1
shraddhamunavalli <munavallishraddha4@gmail.com>
2026-01-18 21:40:29 +0530

    Fix test function definition for square

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
-from app import square
-def_test_square():
-result = square(5)
-print("Square of 5 =", result)
-assert result == 25
+ from app import square
+
+def test_square():
+    result = square(5)
+    print("Square of 5 =", result)
+    assert result == 25
